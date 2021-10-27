0b6227236f7686fe832fdef90808b4d4c680badd
sarathpariyarath <sarath.pariyarath@gmail.com>
2021-10-27 11:24:04 +0530

    Final

--- UITableView/ViewController.swift
+++ UITableView/ViewController.swift
@@ -35,25 +35,25 @@ class ViewController: UIViewController {
     ]
     
     var number = [
-        "+91 7558 042 864",
-        "+91 9496 250 520",
-        "+91 9774 531 501",
-        "+91 3784 367 434",
-        "+91 9893 929 733",
-        "+91 9328 903 939",
-        "+91 9308 439 093",
-        "+91 9034 903 929",
-        "+91 9034 903 929",
-        "+91 9034 903 929",
-        "+91 9034 903 929",
-        "+91 9034 903 929",
-        "+91 9774 531 501",
-        "+91 9774 531 501",
-        "+91 9774 531 501",
-        "+91 9774 531 501",
-        "+91 9774 531 501",
-        "+91 9774 531 501",
-        "+91 9774 531 501"
+            "+91 7558 042 864",
+            "+91 9496 250 520",
+            "+91 9774 531 501",
+            "+91 3784 367 434",
+            "+91 9893 929 733",
+            "+91 9328 903 939",
+            "+91 9308 439 093",
+            "+91 9034 903 929",
+            "+91 9034 903 929",
+            "+91 9034 903 929",
+            "+91 9034 903 929",
+            "+91 9034 903 929",
+            "+91 9774 531 501",
+            "+91 9774 531 501",
+            "+91 9774 531 501",
+            "+91 9774 531 501",
+            "+91 9774 531 501",
+            "+91 9774 531 501",
+            "+91 9774 531 501"
     ]
     override func viewDidLoad() {
         navigationController?.navigationBar.prefersLargeTitles = true
@@ -66,38 +66,37 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         
         
-        //        Ask for permission
+        //Ask for permission
         
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .sound]) { granted, error in
         }
         
-        //        Content for Notification
+        //Content for Notification
         let content = UNMutableNotificationContent()
         content.title = "Notification"
         content.body = "This is a random notification by this app :)"
         
         
-        //    triggering notification
+        //Triggering notification
         
-        let date = Date().addingTimeInterval(5)
+        let date = Date().addingTimeInterval(10)
         let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
         
         
-        //        create request'
+        //create request'
         let uuIdString = UUID().uuidString
         let request = UNNotificationRequest (identifier: uuIdString, content: content, trigger: trigger)
         
         
-        //        add request
+        //Add request
         center.add(request) { Error in}
     }
     
     
 }
-
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -110,9 +109,6 @@ extension ViewController: UITableViewDataSource {
         
         return cell
     }
-    
-    
-    
 }
 
 extension ViewController: UITableViewDelegate {
